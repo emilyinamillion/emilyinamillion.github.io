5542aec5ad6733a394b4d6e77c71fbb0acad718b
Emily Barry <emilybarry@emilys-mbp.lan>
2020-09-12 17:50:52 -0700

    bookmarking changes on a box I cannot understand

---
+++ index.js
@@ -0,0 +1,23 @@
+let img;
+
+function setup() {
+  createCanvas(10000, 10000);
+  frameRate(5);
+  let img = loadImage('ersatzGirl/IMG_1294.png');
+  console.log(img.height);
+  // background(88, 24, 69);
+};
+
+function draw() {
+  // beginShape();
+  createCanvas(10000, 10000);
+  // let img = loadImage('ersatzGirl/IMG_1294.png');
+  // image(img, 0, 0);
+  beginShape();
+  fill(255, 192, 203, 70)
+  strokeWeight(4);
+  stroke(51);
+  circle(mouseX, mouseY, 100)
+  endShape(close);
+
+};
