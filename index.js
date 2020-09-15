1a2d075570706665d100066ecfce2bc8eda8273d
Emily Barry <emilybarry@emilys-mbp.lan>
2020-09-14 19:01:28 -0700

    adds a little p5.js

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 let img;
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function mouseMoved() {
+  // beginShape();
+  fill(255, 192, 203, 70)
+  strokeWeight(4);
+  stroke(51);
+  bubble = circle(mouseX, mouseY, 100)
+
+  endShape(CLOSE);
+  // prevent default
+  return false;
+}
+
 function setup() {
-  // createCanvas(600, 800);
-  frameRate(5);
-  let img = loadImage('ersatzGirl/IMG_1294.png');
-  console.log(img.height);
-  console.log("here");
+  canvas = createCanvas(windowWidth, windowHeight);
+  frameRate(200);
+  // let img = loadImage('ersatzGirl/IMG_1294.png');
+  canvas.position(0,0);
+  canvas.style("z-index", -1);
   // background(88, 24, 69);
 };
 
 function draw() {
-  // beginShape();
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0,0);
+  canvas.style("z-index", -1);
   // createCanvas(10000, 10000);
   // let img = loadImage('ersatzGirl/IMG_1294.png');
   // image(img, 0, 0);
-  beginShape();
-  fill(255, 192, 203, 70)
-  strokeWeight(4);
-  stroke(51);
-  circle(mouseX, mouseY, 100)
-  endShape(CLOSE);
+  mouseMoved();
+  // beginShape();
+  // fill(255, 192, 203, 70)
+  // strokeWeight(4);
+  // stroke(51);
+  // circle(mouseX, mouseY, 100)
+  // endShape(CLOSE);
 
 };
